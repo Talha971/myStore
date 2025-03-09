74e46e0f0974f9cea016f03f278e899dbf02c855
Talha971 <talhatahir971@gmail.com>
2025-03-09 17:00:03 +0500

    Create page added

---
+++ Pages/Clients/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Crud__Asp.net_core_.Pages.Clients
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
